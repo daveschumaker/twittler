273f98ed96dd09de6bcf2019ccec84119854a9b0
Dave Schumaker <daveschumaker@gmail.com>
2015-06-23 08:57:49 -0700

    Add current date and time to user generates tweets.

--- js/data_generator.js
+++ js/data_generator.js
@@ -16,6 +16,7 @@ window.users = Object.keys(streams.users);
 // utility function for adding tweets to our data structures
 var addTweet = function(newTweet){
   var username = newTweet.user;
+
   streams.users[username].push(newTweet);
   streams.home.push(newTweet);
 };
@@ -65,5 +66,6 @@ var writeTweet = function(message){
   var tweet = {};
   tweet.user = visitor;
   tweet.message = message;
+  tweet.created_at = new Date();
   addTweet(tweet);
 };
